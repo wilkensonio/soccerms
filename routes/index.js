5f86d36e633870a08eeb74401541932346239610
Kelvin Chaguay <chaguayk1@southernct.edu>
2024-11-27 23:56:57 -0800

    I commit this change for the match.html

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,13 @@ const playersRoutes = require('./players');
 const clubsRoutes = require('./clubs');
 const transfers = require('./transfers');  
 const fans = require('./fans');
+const matchesRoutes = require('./matches');
  
 router.use('/players', playersRoutes);
 router.use('/clubs', clubsRoutes);
 router.use('/transfers', transfers);
 router.use('/fans', fans);
+router.use('/matches', matchesRoutes);
+
 
 module.exports = router;
