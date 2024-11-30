ddf5323bff94e3ad5c6257862296dea735176235
aerron21 <warda21@southernct.edu>
2024-11-30 15:39:30 -0500

    competition page +  api

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: ' ',
-    password: ' ',
+    user: '',
+    password: '',
     database: 'playerdb'
 });
 
